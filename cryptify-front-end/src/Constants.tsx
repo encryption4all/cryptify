2abf13fbe9d4b45fda39170bfa75c19955a93246
Leon <l.botros@cs.ru.nl>
2023-06-05 12:32:10 +0200

    feat: use new stable PKG url

--- cryptify-front-end/src/Constants.tsx
+++ cryptify-front-end/src/Constants.tsx
@@ -12,9 +12,7 @@ export const SMOOTH_TIME: number = 2;
 
 const prod = process.env.NODE_ENV === "production";
 
-export const PKG_URL = `https://${
-  prod ? "stable" : "main"
-}.irmaseal-pkg.ihub.ru.nl`;
+export const PKG_URL = "https://postguard.eu/pkg"
 
 export const BACKEND_URL = prod ? "https://cryptify.nl/api/v2" : "";
 
