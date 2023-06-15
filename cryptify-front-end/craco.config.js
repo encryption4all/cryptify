b21cbe4a4f702990c5c755181eab55ef1a38de29
Leon <l.botros@cs.ru.nl>
2023-06-15 15:32:27 +0200

    chore: rename to Yivi

--- cryptify-front-end/craco.config.js
+++ cryptify-front-end/craco.config.js
@@ -1,6 +1,15 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
   webpack: {
     configure: (config) => {
+      config.plugins = [...config.plugins, new NodePolyfillPlugin()];
+      const scopePluginIndex = config.resolve.plugins.findIndex(
+        ({ constructor }) =>
+          constructor && constructor.name === "ModuleScopePlugin"
+      );
+
+      config.resolve.plugins.splice(scopePluginIndex, 1);
       config.resolve.fallback = {
         http: false,
         https: false,
