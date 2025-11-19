733f4d7c2db31658a3a638e5ad0c610186f380c8
Fay <61050421+FayKn@users.noreply.github.com>
2025-11-19 15:22:47 +0100

    External config file
    so it can later be replaced with a volume at runtime for the built container

---
+++ cryptify-front-end/public/config.js
@@ -0,0 +1,5 @@
+// runtime configuration for the frontend so it can easily be configured on a prebuilt container
+window.__APP_CONFIG__ = {
+    BACKEND_URL: "https://cryptify.nl/api/v2",
+    PKG_URL: "https://postguard-stable.cs.ru.nl/pkg",
+};
--- cryptify-front-end/src/Constants.tsx
+++ cryptify-front-end/src/Constants.tsx
@@ -1,4 +1,11 @@
-import { browserName, browserVersion, isMobile } from "react-device-detect";
+import {browserName, browserVersion, isMobile} from "react-device-detect";
+
+type ConfigFile = {
+    PKG_URL?: string;
+    BACKEND_URL?: string;
+};
+const rawConfig: unknown = (window as any).__APP_CONFIG__;
+const configFile: ConfigFile = rawConfig && typeof rawConfig === "object" ? (rawConfig as ConfigFile) : {};
 
 // 2GB
 export const MAX_UPLOAD_SIZE: number = 2 * 1000 * 1000 * 1000;
@@ -10,18 +17,16 @@ export const UPLOAD_CHUNK_SIZE: number = 1024 * 1024;
 // progress bar smooth time in seconds.
 export const SMOOTH_TIME: number = 2;
 
-const isStable = process.env.REACT_APP_ENV === "stable";
-
-export const PKG_URL = `https://postguard-${process.env.REACT_APP_ENV}.cs.ru.nl/pkg`
+export const PKG_URL = configFile.PKG_URL ?? `https://postguard-${process.env.REACT_APP_ENV}.cs.ru.nl/pkg`
 
 // Stable: https://cryptify.nl/api/v2
 // Main: https://cryptify.nl/main/api/v2
-export const BACKEND_URL = isStable ? "https://cryptify.nl/api/v2" : "https://cryptify.nl/main/api/v2";
+export const BACKEND_URL = configFile.BACKEND_URL ?? "https://cryptify.nl/api/v2";
 
 export const METRICS_HEADER = {
-  "X-PostGuard-Client-Version": `${browserName}${
-    isMobile ? "(mobile)" : ""
-  },${browserVersion},${process.env.REACT_APP_NAME},${
-    process.env.REACT_APP_VERSION
-  }`,
+    "X-PostGuard-Client-Version": `${browserName}${
+        isMobile ? "(mobile)" : ""
+    },${browserVersion},${process.env.REACT_APP_NAME},${
+        process.env.REACT_APP_VERSION
+    }`,
 };
